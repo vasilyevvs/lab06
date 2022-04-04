2d9aa9345865e31c598a157dcba2c5661153c765
vasilyevvs <danila.vasiliev2002@yandex.ru>
2022-04-04 11:24:46 +0300

    v2.2 test files fix

---
+++ tests/test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 Your Name <your_email>
+
+#include <gtest/gtest.h>
+
+#include <hash_calculation.hpp>
+#include <stdexcept>
+
+TEST(Example, EmptyTest) {
+  bool val = true;
+  EXPECT_EQ(val, true);
+}
